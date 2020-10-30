9c9ba9ea1f916608006bfda18308bc71c9c80644
MarharytaKrasikava <marharytakrasikava@gmail.com>
2020-10-30 22:28:05 +0300

    refactor: code review

--- src/app/video-courses/courses/courses.component.ts
+++ src/app/video-courses/courses/courses.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faPlus, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { Course } from '../../shared/models';
+
+import { mockedCourses } from './mockedCourses';
 
 @Component({
   selector: 'app-courses',
@@ -12,32 +14,7 @@ export class CoursesComponent implements OnInit {
   public addCourseIcon: IconDefinition = faPlus;
 
   public ngOnInit(): void {
-    this.courses = [
-      new Course(
-        '1',
-        'Name tag',
-        new Date('2020-08-09'),
-        45,
-        `Learn about where you can find course description, what
-          information they include, how they work, and details about various
-          components of a course description. Course description report information
-           about a university or college's classes. They're published both in course
-            catalogs that outline degree requirements and in course schedules that
-             contain descriptions for all courses offered during`
-      ),
-      new Course(
-        '2',
-        'Name tag',
-        new Date('2020-10-25'),
-        60,
-        `Learn about where you can find course description, what
-       information they include, how they work, and details about various
-       components of a course description. Course description report information
-        about a university or college's classes. They're published both in course
-         catalogs that outline degree requirements and in course schedules that
-          contain descriptions for all courses offered during`
-      ),
-    ];
+    this.courses = mockedCourses;
   }
 
   public loadCourses(): void {
